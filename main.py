5bd34ea508eafffb334b32324a28fefc6c287a57
NdA66 <nda994@gmail.com>
2020-04-24 19:10:02 +0200

    creazione grafo

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import csv
+import networkx as nx
 
+
+G = nx.DiGraph()
 with open('./dati/01_sites.csv', newline='') as csvfile:
     topology = csv.reader(csvfile, delimiter=' ')
     for row in topology:
-        pass
-        #print(', '.join(row))
-        #print(row)
-        #print(type(row))
+        G.add_node(row[0], name=row[1])
 
 
 with open('./dati/01_topology.csv', newline='') as csvfile:
     topology = csv.reader(csvfile, delimiter=' ')
     for row in topology:
-        pass
-        #print(', '.join(row))
-        #print(row)
-        #print(type(row))
+        G.add_edge(row[0], row[1], cap=int(row[2]))
 
 
 with open('./dati/01_commodities.csv', newline='') as csvfile:
     topology = csv.reader(csvfile, delimiter=' ')
     for row in topology:
-        #print(', '.join(row))
-        print(row)
-        #print(type(row))
+        pass
+
+
