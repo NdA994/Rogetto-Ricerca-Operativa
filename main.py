3395b0f78d53229d04f68110d7bbe36967ad17c6
NdA66 <nda994@gmail.com>
2020-04-24 18:04:43 +0200

    lettura csv e eliminazione intestazione dai suddetti

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("ciao")
+import csv
+
+with open('./dati/01_sites.csv', newline='') as csvfile:
+    topology = csv.reader(csvfile, delimiter=' ')
+    for row in topology:
+        pass
+        #print(', '.join(row))
+        #print(row)
+        #print(type(row))
+
+
+with open('./dati/01_topology.csv', newline='') as csvfile:
+    topology = csv.reader(csvfile, delimiter=' ')
+    for row in topology:
+        pass
+        #print(', '.join(row))
+        #print(row)
+        #print(type(row))
+
+
+with open('./dati/01_commodities.csv', newline='') as csvfile:
+    topology = csv.reader(csvfile, delimiter=' ')
+    for row in topology:
+        #print(', '.join(row))
+        print(row)
+        #print(type(row))
