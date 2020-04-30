0423a98915b986b19a6dfac9c3008b9ef46660d3
NdA66 <nda994@gmail.com>
2020-05-01 01:27:52 +0200

    calcolo length shortest path

--- main.py
+++ main.py
@@ -3,21 +3,42 @@ import networkx as nx
 
 
 G = nx.DiGraph()
+node = []
+edge = []
+comm = []
+
+#NODI
+#Parametro 1: ID nodo
+#Parametro 2: Nome nodo
 with open('./dati/01_sites.csv', newline='') as csvfile:
     topology = csv.reader(csvfile, delimiter=' ')
     for row in topology:
         G.add_node(row[0], name=row[1])
+        node.append(row)
 
-
+#ARCHI
+#Parametro 1: ID nodo sorgente
+#Parametro 2: ID nodo destinazione
+#Parametro 3: Capacità arco
 with open('./dati/01_topology.csv', newline='') as csvfile:
     topology = csv.reader(csvfile, delimiter=' ')
     for row in topology:
         G.add_edge(row[0], row[1], cap=int(row[2]))
+        edge.append(row)
 
-
+#COMMODITIES
+#Parametro 1: ID Sorgente
+#Parametro 2: ID Destinazione
+#Parametro 3: Banda richiesta
+#Parametro 4: Priotrità
 with open('./dati/01_commodities.csv', newline='') as csvfile:
     topology = csv.reader(csvfile, delimiter=' ')
     for row in topology:
-        pass
+        comm.append(row)
 
+#print(node[0][0])
+#print([p for p in nx.all_shortest_paths(G, source=node[0][0], target=node[5][0])])
+
+for row in comm:
+    print(nx.shortest_path_length(G, source=(row[0]), target=row[1]))
 
