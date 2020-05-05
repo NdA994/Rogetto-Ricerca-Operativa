30561e19ba3c6199eb05366f69829b1f7cec2ad3
NdA66 <nda994@gmail.com>
2020-05-05 05:50:10 +0200

    start calcolo soluzione

--- main.py
+++ main.py
@@ -2,48 +2,53 @@ import csv
 
 import networkx as nx
 
-#TO DO: Sistemare l'esecuzione del programma. Funzioni e cose varie.
+# TO DO: Sistemare l'esecuzione del programma. Funzioni e cose varie.
 
 G = nx.DiGraph()
 node = []
 edge = []
 comm = []
+pres = []
 
-#NODI
-#Parametro 1: ID nodo
-#Parametro 2: Nome nodo
+# NODI
+# Parametro 1: ID nodo
+# Parametro 2: Nome nodo
 with open('./dati/05_giorno_commodities.csv', newline='') as csvfile:
     topology = csv.reader(csvfile, delimiter=' ')
     for row in topology:
         G.add_node(row[0], name=row[1])
         node.append(row)
 
-#ARCHI
-#Parametro 1: ID nodo sorgente
-#Parametro 2: ID nodo destinazione
-#Parametro 3: Capacità arco
+# ARCHI
+# Parametro 1: ID nodo sorgente
+# Parametro 2: ID nodo destinazione
+# Parametro 3: Capacità arco
 with open('./dati/05_giorno_topology.csv', newline='') as csvfile:
     topology = csv.reader(csvfile, delimiter=' ')
     for row in topology:
         G.add_edge(row[0], row[1], cap=int(row[2]))
         edge.append(row)
 
-#COMMODITIES
-#Parametro 1: ID Sorgente
-#Parametro 2: ID Destinazione
-#Parametro 3: Banda richiesta
-#Parametro 4: Priotrità
+# COMMODITIES
+# Parametro 1: ID Sorgente
+# Parametro 2: ID Destinazione
+# Parametro 3: Banda richiesta
+# Parametro 4: Priotrità
 i = 0
 with open('./dati/05_giorno_commodities.csv', newline='') as csvfile:
     topology = csv.reader(csvfile, delimiter=' ')
     for row in topology:
         row[4] = i
         comm.append(row)
-        i = i+1
+        i = i + 1
 
-with open('./out2/05_giorno_commodities.csv', newline='') as csvfile:
+with open('./out2/resPath.csv', newline='') as csvfile:
     topology = csv.reader(csvfile, delimiter=' ')
     for row in topology:
-        row[4] = i
-        comm.append(row)
-        i = i+1
+        if int(row[1]) == 1:
+            pres.append(row)
+
+for row in pres:
+    comm.remove(comm[int(row[0])])
+print(comm)
+print(pres)
