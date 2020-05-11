7957a70c10eb48f9a32851aa0847fb5a7529e7b6
NdA66 <nda994@gmail.com>
2020-05-12 01:25:35 +0200

    change input

--- solution.py
+++ solution.py
@@ -9,7 +9,7 @@ comm = []
 # NODI
 # Parametro 1: ID nodo
 # Parametro 2: Nome nodo
-with open('./preSolve2/05_giorno_topology.csv', newline='') as csvfile:
+with open('./dati/01_sites.csv', newline='') as csvfile:
     topology = csv.reader(csvfile, delimiter=' ')
     for row in topology:
         G.add_node(row[0], name=row[1])
@@ -19,7 +19,7 @@ with open('./preSolve2/05_giorno_topology.csv', newline='') as csvfile:
 # Parametro 1: ID nodo sorgente
 # Parametro 2: ID nodo destinazione
 # Parametro 3: Capacità arco
-with open('./preSolve2/05_giorno_topology.csv', newline='') as csvfile:
+with open('./dati/01_topology.csv', newline='') as csvfile:
     topology = csv.reader(csvfile, delimiter=' ')
     for row in topology:
         G.add_edge(row[0], row[1], cap=int(row[2]))
@@ -31,11 +31,13 @@ with open('./preSolve2/05_giorno_topology.csv', newline='') as csvfile:
 # Parametro 3: Banda richiesta
 # Parametro 4: Priotrità
 # Parametro 5: ID Commodities
-with open('./preSolve2/05_giorno_commodities.csv', newline='') as csvfile:
+with open('./dati/01_commodities.csv', newline='') as csvfile:
     topology = csv.reader(csvfile, delimiter=' ')
     for row in topology:
         comm.append(row)
 
+
+'''
 for row in comm:
     Sk = nx.shortest_path(G, source=(row[0]), target=row[1])
     for incr in range(len(Sk)-1):
@@ -46,3 +48,4 @@ for row in comm:
     print(row)
 
 print(edge)
+'''
